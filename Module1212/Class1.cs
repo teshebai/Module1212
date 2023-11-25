72a426e07393f953c9dae566ed794219112002a7
Teshebayev <eraliteshebai2002gmail.com>
2023-11-25 10:48:35 +0600

    qw

---
+++ Module1212/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1212
+{
+    public delegate void PropertyeventHandler(object sender, PropertyeventArgs e);
+
+    public interface iPropertychanged
+    {
+        event PropertyeventHandler Propertychanged;
+    }
+
+    public class PropertyeventArgs : EventArgs
+    {
+        public string PropertyName { get; private set; }
+
+        public PropertyeventArgs(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+    public class SampleClass : iPropertychanged
+    {
+        private string sampleProperty;
+        public event PropertyeventHandler Propertychanged;
+
+        public string SampleProperty
+        {
+            get { return sampleProperty; }
+            set
+            {
+                if (sampleProperty != value)
+                {
+                    sampleProperty = value;
+                    OnPropertychanged("SampleProperty");
+                }
+            }
+        }
+
+        protected virtual void OnPropertychanged(string propertyName)
+        {
+            Propertychanged?.Invoke(this, new PropertyeventArgs(propertyName));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleObject = new SampleClass();
+            sampleObject.Propertychanged += SampleObject_Propertychanged;
+
+            sampleObject.SampleProperty = "New Value";
+            Console.ReadKey();
+        }
+
+        private static void SampleObject_Propertychanged(object sender, PropertyeventArgs e)
+        {
+            Console.WriteLine($"Property changed: {e.PropertyName}");
+        }
+    }
+
+}
---
+++ Module1212/Class2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1212
+{
+
+    class Program
+    {
+        public delegate double MathOperation(double a, double b);
+        public static class MathOperations
+        {
+            public static double Add(double a, double b) => a + b;
+            public static double Subtract(double a, double b) => a - b;
+            public static double Multiply(double a, double b) => a * b;
+            public static double Divide(double a, double b) => a / b;
+        }
+        public static double PerformOperation(double x, double y, MathOperation operation)
+        {
+            return operation(x, y);
+        }
+        static void Main(string[] args)
+        {
+            MathOperation add = MathOperations.Add;
+            MathOperation subtract = MathOperations.Subtract;
+            MathOperation multiply = MathOperations.Multiply;
+            MathOperation divide = MathOperations.Divide;
+
+            double result1 = PerformOperation(10, 5, add);
+            Console.WriteLine($"Addition: {result1}");
+
+            double result2 = PerformOperation(10, 5, subtract);
+            Console.WriteLine($"Subtraction: {result2}");
+
+
+            MathOperation lambdaMultiply = (a, b) => a * b;
+            double lambdaResult = PerformOperation(10, 5, lambdaMultiply);
+            Console.WriteLine($"Lambda Multiply: {lambdaResult}");
+
+
+            MathOperation operationsChain = add + subtract + multiply + divide;
+            double chainResult = PerformOperation(10, 5, operationsChain);
+            Console.WriteLine($"Operations Chain Result: {chainResult}");
+            Console.ReadKey();
+        }
+    }
+
+}
